5f0dad2d8c22912587d2ac74d673be00a3d3a021
Tony <vangt90@gmail.com>
2016-03-14 15:08:37 -0400

    paperclip profile image

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -41,6 +41,6 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:fname, :lname, :job_title)
+    params.require(:profile).permit(:fname, :lname, :job_title, :image)
   end
 end
