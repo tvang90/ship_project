60de0a1d3f404e0ca1ca5bc5b00ffc78a8404626
Tony <vangt90@gmail.com>
2016-03-14 20:43:48 -0400

    updated signup, edit and link to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
   has_many :boats
   has_many :jobs
 
+  has_attached_file :image, styles: {large: "600x600>", medium: "300x300>", small: "150x150#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
+
 end
